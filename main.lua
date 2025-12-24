816362fb957173133d35ee8c5504dbbdd79b705f
shwermintube-wq <shwermintube@gmail.com>
2025-12-24 07:44:34 -0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,95 @@
+local home = require("lib.locations.home")
+local move = require("lib.movement")
+local safety = require("lib.safety")
+local net = require("lib.net")
+local entrance = require("lib.locations.entrance")
+local bank = require("lib.locations.bank")
+
+local ENTRANCE = entrance.pos
+local HOME = home.load()
+
+move.sync()
+
+local function handleCommand(sender, msg)
+    local args = {}
+    print("Debug", msg)
+    for word in string.gmatch(msg, "%S+") do
+        table.insert(args, word)
+    end
+
+    local msg = args[1]
+
+    if msg == "up" then
+        local dist = tonumber(args[2])
+        for i=1, dist do
+            move.up(1)
+        end
+        return "Moved up"..dist
+
+    elseif msg == "down" then
+        local dist = tonumber(args[2])
+        for i=1, dist do
+            move.down(1)
+        end
+        return "Moved down"..dist
+
+     elseif msg == "forward" then
+        local dist = tonumber(args[2])
+        for i=1, dist do
+            move.forward(1)
+        end
+        return "Moved forward"..dist
+        
+         elseif msg == "back" then
+        local dist = tonumber(args[2])
+        for i=1, dist do
+            move.back(1)
+        end
+        return "Moved back"..dist
+
+    
+    elseif msg == "left" then
+        local amount = tonumber(args[2])
+        for i=1, amount do
+            move.left()
+        end
+        return "Turned left"..amount
+
+    elseif msg == "right" then
+        local amount = tonumber(args[2])
+        for i=1, amount do
+            move.right()
+        end
+        return "Turned right"..amount
+
+    elseif msg == "detectUp" then
+        move.dUp()
+    elseif msg == "detectDown" then
+        move.dDown()
+
+    elseif msg == "diDown" then
+        move.digDown()
+    elseif msg == "diUp" then
+        move.digUp()
+
+    elseif msg == "drop" then
+        move.drop()
+        return "Dropped"
+    elseif msg == "dropUp" then
+        move.dropUp()
+        return "Dropped"
+    elseif msg == "dropDown" then
+        move.dropDown()
+        return "Dropped"
+
+    elseif msg == "entrance" then
+        move.entrance()
+
+    elseif msg == "bank" then
+        move.findX(bank.x)
+        move.findZ(bank.z)
+        move.findY(bank.y)
+    end
+end -- End of handler
+
+net.listen(handleCommand)
